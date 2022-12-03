4ec37d845975f532f24cfb2fb9b27b0b60303e30
consultorsandro <consultorsandro@hotmail.com>
2022-12-03 05:49:06 -0300

    Atualiza exercícios

---
+++ 04_Relacionais.rb
@@ -0,0 +1,20 @@
+# Esta aula trata dos operadores relacionais, ou resposta bolleana à uma comparação
+
+#Exemplo de operaçao com operador relacional
+# Se verdadeiro, executa o primeiro bloco if
+
+if 1 >= 2
+  puts "a"
+else
+  puts "b" # imprime porque a resposta ao if é false
+end
+
+#Operadores aritméticos
+# Operaçoes básicas da matemática e obtenção do resto
+
+puts a = 1 + 2
+puts b = 1 * 2
+puts c = 2 * 3
+puts d = 3 / 2
+puts e = 3 % 2 #resto 1, muito utilizdo para saber se número é par ou impar
+
