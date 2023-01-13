5288987547f9a16fa103c051bc63eb432c372cf7
consultorsandro <consultorsandro@hotmail.com>
2023-01-13 08:54:58 -0300

    Arquivos deletados do módulo 02 e projeto teste no mod_03

--- modulo_02/10_pessoa.rb
+++
@@ -1,9 +0,0 @@
-# Esta aula trata da criação de classes
-# O identificador deve começar com letra maiúscula por padrão
-# Usa-se camelCase para palavras compostas: PessoaFísica
-class Pessoa
-  def falar # define o método falar
-    "Estou falando"
-  end # fim do bloco do met fala
-
-end
