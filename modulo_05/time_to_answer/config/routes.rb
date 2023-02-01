1449c2c297551264cf65eae1616358f13681ec39
consultorsandro <consultorsandro@hotmail.com>
2023-02-01 08:36:21 -0300

    Projeto Modulo 5 - time_to_answer

---
+++ modulo_05/time_to_answer/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
+  get 'inicio', to:'welcome#index'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
