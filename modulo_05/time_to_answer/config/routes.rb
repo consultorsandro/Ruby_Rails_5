4914a06fd68424c396a10091cf645e4722202896
consultorsandro <consultorsandro@hotmail.com>
2023-02-02 08:49:20 -0300

    Reinício da tarefa

--- modulo_05/time_to_answer/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'welcome#index'
-  get 'inicio', to:'welcome#index'
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
